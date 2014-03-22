e4a96b30e023abff9a047b5c43ba1345a20f2f1c
Fyodor Y <fygrave@o0o.nu>
2014-03-23 02:58:20 +0800

    Fix for the query interface

--- lib/queries/whois_query.py
+++ lib/queries/whois_query.py
@@ -19,36 +19,60 @@ class WhoisQuery():
         self.es_type = es_type
 
     def validate(self, s):
-        return filter(lambda x: x in string.ascii_letters +string.digits+':'+'.'+'*'+ '+', s)
+        return filter(lambda x: x in string.ascii_letters +string.digits+'-'+':'+'.'+'*'+ '+' + '[' + ']' + '|' + '@' + '_', s)
 
     def list_to_str(self,l):
         if len(l) == 0:
             return ""
-        return reduce(lambda x, y: "%s\r\n\r\n%s"%(x,y), map(self.printable_entry,  l))
+        return reduce(lambda x, y: "%s\r\n%s"%(x,y), map(self.printable_entry,  l))
 
     def printable_entry(self,  val):
        v = val['_source']
-       return "%s,"% (json.dumps(v))
+       
+       
+       return "%s\r\n%s"%(reduce(lambda x,y: "%s, %s"% (x,y), v.keys()), reduce (lambda x, y: "%s, %s" % (x,y), v.values()))
+       #return "%s,"% (json.dumps(v))
 
     def whois_query(self, query):
+        start_num = 0
+        size_num = 100 # defaults
         query = self.validate(query)
         queries = []
+        day = '%.4i%.2i%.2i%.2i'% (datetime.datetime.now().year, datetime.datetime.now().month, datetime.datetime.now().day, datetime.datetime.now().hour)
+        if query.find('@') != -1:
+            day = query.split('@', 1)
+            query = day[1]
+            day = day[0]
         qqq = query.split('+')
         for quer in qqq:
-            qq = quer.split(':')
+            qq = quer.split(':', 1)
             field = '_all'
             if len(qq) > 1:
                 field = qq[0]
                 query = qq[1]
-                q = pyes.query.TextQuery(field, query)
+		q = None
+                if field == "count":
+                    size_num = int(query)
+                    continue
+                elif field == "start":
+                    start_num = int(query)
+                    continue
+		elif query.find('[') != -1:
+			from_d = query[1:query.find('|')]
+			to_d = query[query.find('|') + 1:query.find(']')]
+			print from_d
+			print to_d
+			q = pyes.query.RangeQuery(qrange=pyes.query.ESRange(field, to_value=to_d,  from_value=from_d))
+		else:	
+			q = pyes.query.TextQuery(field, query)
                 queries.append(q)
         to_return = ""
         try:         
             bq = pyes.query.BoolQuery(must=queries)
-            rez = self.es.search(query=bq, indices=self.index, doc_types=self.es_type)
-            to_return = "Total %s matches\r\n%s" % (rez.count(), self.list_to_str(rez._results['hits']['hits']))
+            rez = self.es.search(pyes.query.Search(query=bq, start=start_num, size=size_num), indices="%s-%s"% (self.index, day), doc_types=self.es_type)
+            to_return = "Result, Total %s matches\r\n%s" % (rez.count(), self.list_to_str(rez._results['hits']['hits']))
         except Exception, e:
-            to_return = str(e)
+            to_return = "Result, Error: %s\r\n" % str(e)
 
 
         return to_return
@@ -57,12 +81,11 @@ class WhoisQuery():
 
 if __name__ == "__main__":
     import os
-    import IPy
     import sys
     import ConfigParser
     config = ConfigParser.RawConfigParser()
     config.read("../../etc/whois-server.conf")
-    query_maker = WhoisQuery(int(config.get('whois_server','redis_db')), config.get('whois_server','prepend_to_keys'))
+    query_maker = WhoisQuery('localhost:9200', 'searchindex', 'searchindex-type')
 
     def usage():
         print "arin_query.py query"
@@ -72,14 +95,4 @@ if __name__ == "__main__":
         usage()
 
     query = sys.argv[1]
-    ip = None
-    try:
-        ip = IPy.IP(query)
-    except:
-        pass
-
-
-    if ip:
-        print(query_maker.whois_ip(ip))
-    else:
-       print(query_maker.whois_asn(query))
+    print(query_maker.whois_query(query))
